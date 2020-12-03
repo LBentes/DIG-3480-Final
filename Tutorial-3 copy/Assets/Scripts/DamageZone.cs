ff13374ad51022ac38511d5e6f67bc1fbe52dc7d
LBentes <60331057+LBentes@users.noreply.github.com>
2020-12-03 16:05:56 -0500

    FIrst Commit

---
+++ Tutorial-3 copy/Assets/Scripts/DamageZone.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageZone : MonoBehaviour
+{
+    public RubyController player;
+    public float speedDamage = 1.0f;
+    public float speedOriginal = 3.0f;
+    private IEnumerator speedChange;
+    //public RubySpeed = GameObject.Find("Ruby").GetComponent<RubyController>().speed;
+        void OnTriggerStay2D(Collider2D other)
+    {
+        RubyController controller = other.GetComponent<RubyController >();
+
+        if (controller != null)
+        {
+            controller.ChangeHealth(-1);
+            //ChangeDamageSpeed();
+            speedChange = ChangeDamageSpeed();
+            StartCoroutine(speedChange);
+        }
+    }
+    IEnumerator ChangeDamageSpeed()
+    {
+        player.speed = speedDamage;
+        yield return new WaitForSeconds(2.0f);
+        player.speed = speedOriginal;
+    }
+}
